d9da05fc232fbb8ebfaecaa90638f16cb47dfb7e
Akin-Genc0 <akingenc212@gmail.com>
2025-05-09 16:09:58 +0100

    Add basic timer functionality that starts on sentence generation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('.str');
 const content = document.querySelector('.hold');
 const inp = document.querySelector('#userInput');
+const timer =  document.querySelector("p");
 
 //array with random sentences
 const randomSen = [
@@ -18,7 +19,15 @@ function getRandomSen(arr) {
     const i = Math.floor(Math.random() * arr.length);
     return arr[i]; 
 }
+ 
+let setTime;  //global varibal
 
+//timer function
+    function myTimer() {
+        let text = timer.textContent;
+        let increment  =  Number(text) + 1 
+        timer.textContent > 0 ? timer.textContent = increment : 0;
+    }
 
 
 //start button
@@ -27,17 +36,21 @@ startBtn.addEventListener('click', function(){
     content.textContent = "";
     const sentenceText = getRandomSen(randomSen);
     content.textContent = sentenceText;
-    
-   
+
+    timer.textContent = 1;
+    setTime = setInterval(myTimer, 1000);
+
  
 //check input in real time
    inp.addEventListener("input" , function() {
 
     if(inp.value === sentenceText){
-        alert("youwin")
+        alert("you win");
+        clearInterval(setTime)
         } 
 });
 
+ 
 });
 
 
